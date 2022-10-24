4744221167335c1bea2497a7174a195d54aaefa0
Raymond <magicpalmtree.space@gmail.com>
2022-10-24 11:04:20 -0400

    Feat: Recurring pulse bot
    the bot automatically launches again every 2 days

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,14 @@
 module.exports = {
 	name: 'ready',
-	once: true,
 	execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
+		
+		const time = 3600 * 24 * 1000 * (process.env.RELAUNCH_DATE ?? 1);
+		console.log(time)
+		setInterval(async () => {
+			await client.destroy();
+			await client.login(process.env.TOKEN);
+			console.log(`Ready! Restarted in as ${client.user.tag}`)
+		}, time)
 	},
 }
