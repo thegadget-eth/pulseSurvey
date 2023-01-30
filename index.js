2eff9e5373fff66e47eb4ea136cdebcce3702aa3
Raymond <magicpalmtree.space@gmail.com>
2023-01-30 16:39:11 -0500

    Feat: toggle extraction status

--- index.js
+++ index.js
@@ -67,31 +67,35 @@ const checkBotStatus = async (settings) => {
   return await Promise.all(promises);
 };
 
+const toggleExtraction = async (setting, status) => {
+  const { guildId } = setting;
+  setting.isInProgress = status;
+  await updateGuild(guildId, setting);
+};
+
 const app = async () => {
   // fetch all guild settings
   await discordLogin();
   await connectDB();
+  // only fetch connected guilds
   const settings = await fetchSettings();
   await checkBotStatus(settings);
   promises = settings.map(async (setting) => {
     const { guildId, name } = setting;
-    const guild = client.guilds.cache.get(guildId);
-    if (guild) {
-      // fetch missed messages from discord
-      const messages = await fetch(setting);
-      // insert messages to the database
-      const numberOfNewMessage = await insertMessages(guildId, messages);
 
-      if (numberOfNewMessage === 0) {
-        console.log("No new messages are fetched from ", name);
-      } else {
-        console.log(
-          `Successfully stored ${numberOfNewMessage} messages from`,
-          name
-        );
-      }
+    await toggleExtraction(setting, true);
+    // fetch missed messages from discord
+    const messages = await fetch(setting);
+    // insert messages to the database
+    const numberOfNewMessage = await insertMessages(guildId, messages);
+    await toggleExtraction(setting, false);
+    if (numberOfNewMessage === 0) {
+      console.log("No new messages are fetched from ", name);
     } else {
-      console.log(`The bot is disconnected from the server named ${name}`);
+      console.log(
+        `Successfully stored ${numberOfNewMessage} messages from`,
+        name
+      );
     }
   });
   await Promise.all(promises);
